88a2e8ff2d1f9bc3a933b5a3ebefb645d22240c2
DarkerCz <j.valdauf@gmail.com>
2023-03-17 15:50:21 +0100

    Reseni 1. ukolu

--- 1_ukol/kod.py
+++ 1_ukol/kod.py
@@ -3,12 +3,12 @@ import time
 import sqlite3
 import pandas as pd
 
-shutil.copy('../test.db', 'db_1_ukol.db')
+shutil.copy('../test.db', 'db_1_ukol_sqlite.db')
 
 # SQLite3
 try:
     start = time.time()
-    con = sqlite3.connect("db_1_ukol.db")
+    con = sqlite3.connect("db_1_ukol_sqlite.db")
 
     cur = con.cursor()
     cur.execute("UPDATE smlouvy set platnost_do = DATE(substr(platnost_do, 0,5) || '-' || substr(platnost_do, 9,2) || '-' || substr(platnost_do, 6,2));")
@@ -21,12 +21,12 @@ except Exception as e:
     print("SQL: Chyba pri zpracovani: {}".format(e))
 
 
-shutil.copy('../test.db', 'db_1_ukol.db')
+shutil.copy('../test.db', 'db_1_ukol_pandas.db')
 
 # Pandas
 try:
     start = time.time()
-    con = sqlite3.connect("db_1_ukol.db")
+    con = sqlite3.connect("db_1_ukol_pandas.db")
     smlouvy_df = pd.read_sql_query("select * from smlouvy", con)
     smlouvy_df["platnost_do"] = pd.to_datetime(smlouvy_df["platnost_do"], format='%Y-%d-%m').dt.strftime('%Y-%m-%d')
     smlouvy_df.to_sql(name='smlouvy', con=con, if_exists='replace')
